d2debc3ea0b19c55b447737b3d0c783cd947d06a
Corinne <CorinneKogut@gmail.com>
2024-01-12 10:29:50 -0500

    data model tweaks

--- load_data.py
+++ load_data.py
@@ -1,17 +1,27 @@
 import pandas as pd
 from sqlalchemy import create_engine
-from web_app import db, ssh_tunnel
+from sshtunnel import SSHTunnelForwarder
 from config import *
+
+
+ssh_tunnel = SSHTunnelForwarder(
+        (ssh_host, port),  # replace with your SSH server details
+        ssh_username=ssh_username,
+        ssh_password=ssh_password,
+        remote_bind_address=('localhost', 3306)  # replace with your MySQL server details
+    )
+
+ssh_tunnel.start()
 
 
 # This function handles the loading of movie data into the database.
 def load_movies_data():
     # Read the CSV
-    movies_df = pd.read_csv('csv file')
+    movies_df = pd.read_csv('notebooks/DB_models/DB_genres.csv')
 
-    # Add a 'year' column if not present in CSV
-    if 'year' not in movies_df.columns:
-        movies_df['year'] = None
+    # # Add a 'year' column if not present in CSV
+    # if 'year' not in movies_df.columns:
+    #     movies_df['year'] = None
 
     # Connect to MYSQL database
     # pymysql: Specifies the MySQL driver to be used for the connection.
@@ -21,7 +31,9 @@ def load_movies_data():
 
     # Insert data into the 'movies' table
     # to_sql method to insert the data from the Pandas DataFrame into the 'movies' table in the MySQL database.
-    movies_df.to_sql('movies', con=engine, if_exists='replace', index=False)
+    movies_df.to_sql('movie_genre', con=engine, if_exists='append', index=False)
+
+    ssh_tunnel.close()
 
 
 # function is executed only when the script is run directly, not when it is imported as a module.
